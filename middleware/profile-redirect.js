46bca409fcf22341eb6ccaa5fa658da85c82fcbf
Tanat <ttanat@protonmail.com>
2020-05-02 04:57:32 +0700

    Fix to check if user is logged in

--- middleware/profile-redirect.js
+++ middleware/profile-redirect.js
@@ -1,5 +1,5 @@
 export default async function({ $auth, params, redirect }) {
-  if ($auth && $auth.user.username === params.username) {
+  if ($auth.loggedIn && $auth.user.username === params.username) {
     redirect('/profile')
   }
 }
