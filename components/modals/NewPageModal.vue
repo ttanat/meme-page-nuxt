ddce40e7b9abeb886fea3101fc3af3a087a800e1
Tanat <ttanat@protonmail.com>
2020-05-27 01:06:30 +0700

    Can create new meme page now and add to auth user

--- components/modals/NewPageModal.vue
+++ components/modals/NewPageModal.vue
@@ -8,7 +8,7 @@
         <div class="modal-body">
           <label>Page Name</label>
           <br>
-          <input v-model.trim="pageName" @keyup="pageNameValidChars" :class="{'is-invalid': turnRed}" type="text" class="input-form" maxlength="64" autocomplete="off">
+          <input v-model.trim="pageName" @keyup="pageNameValid" :class="{'is-invalid': turnRed}" type="text" class="input-form" maxlength="64" autocomplete="off">
           <small ref="smallPageName" :class="{red: turnRed}">{{ smallPageNameText }}</small>
           <br><br>
           <label>Display Name <span class="text-muted" style="font-size: 14px;"><i class="far fa-question-circle" data-toggle="tooltip" title="Page name will be shown if display name is empty"></i></span></label>
@@ -17,16 +17,16 @@
           <div class="form-row">
             <div class="col-sm-5 mb-2">
               <label><small>Who can see posts</small></label>
-              <select v-model="pagePrivacy" class="custom-select custom-select-sm mr-sm-2" required>
-                <option value="1">Public (Everyone)</option>
-                <option value="0">Private (Subscribers)</option>
+              <select v-model="pagePrivate" class="custom-select custom-select-sm mr-sm-2" required>
+                <option :value="false">Public (Everyone)</option>
+                <option :value="true">Private (Subscribers)</option>
               </select>
             </div>
             <div class="col-sm-5">
               <label><small>Who can post</small></label>
               <select v-model="pagePermissions" class="custom-select custom-select-sm mr-sm-2" required>
-                <option value="1">Subscribers</option>
-                <option value="0">Only me</option>
+                <option :value="true">Subscribers</option>
+                <option :value="false">Only me</option>
               </select>
             </div>
           </div>
@@ -34,7 +34,16 @@
         <div class="modal-footer">
           <span><small>Maximum 3 per user</small>&emsp;</span>
           <button type="button" class="btn btn-secondary modal-btn" data-dismiss="modal">Cancel</button>
-          <button ref="createBtn" @click="create" :class="{'not-allowed': turnRed || !pageName}" type="button" class="btn btn-primary modal-btn" disabled>Create</button>
+          <button
+            @click="create"
+            :disabled="disableCreateBtn"
+            :class="{'not-allowed': disableCreateBtn}"
+            type="button"
+            class="btn btn-primary modal-btn"
+          >
+            <font-awesome-icon v-if="creating" :icon="['fas', 'circle-notch']" spin />
+            <template v-else>Create</template>
+          </button>
         </div>
       </div>
     </div>
@@ -53,56 +62,91 @@ export default {
       smallPageNameText: "Letters, numbers, and underscores only.",
       TAKEN_PAGE_NAMES: [],
       pageDisplayName: "",
-      pagePrivacy: 1,
-      pagePermissions: 1
+      pagePrivate: false,
+      pagePermissions: true,
+      maximum: false,
+      maximumText: "Maximum number of pages created",
+      creating: false
+    }
+  },
+  computed: {
+    disableCreateBtn() {
+      return this.turnRed || !this.pageName
     }
   },
   methods: {
-    pageNameValidChars() {
+    pageNameValid() {
       this.turnRed = false
       this.smallPageNameText = this.defaultText
       const check = !!this.pageName.match(/^[a-z0-9_]+$/i) // Check if all characters are valid
-      this.$refs.createBtn.disabled = !check
       if (!check) this.invalidateForm()
       return check
     },
     invalidateForm(message=null) {
       this.smallPageNameText = message ? message : this.defaultText
-      this.$refs.createBtn.disabled = true
       this.turnRed = !!this.pageName // Don't turn red if field is empty
     },
-    pageNameTaken() { // Prevents a frenzy of requests with the same page name
+    pageNameTaken() { // Prevents resubmission of form with same page name
       const taken = this.TAKEN_PAGE_NAMES.includes(this.pageName.toLowerCase())
       if (taken) this.invalidateForm(this.nameTakenText)
       return taken
     },
-    validateForm() {
-      return this.pageNameValidChars() && !this.pageNameTaken() && (this.pagePrivacy == 0 || this.pagePrivacy == 1) && (this.pagePermissions == 0 || this.pagePermissions == 1)
+    checkMaximum() {
+      if (this.maximum) this.invalidateForm(this.maximumText)
+      return this.maximum
     },
-    create() {
+    validateForm() {
+      return this.pageNameValid() && !this.pageNameTaken() && !this.checkMaximum() && typeof this.pagePrivate === "boolean" && typeof this.pagePermissions === "boolean"
+    },
+    async create() {
       if (this.validateForm()) {
-        data = new FormData()
-        data.set("name", this.pageName)
-        data.set("displayName", this.pageDisplayName)
-        data.set("privacy", this.pagePrivacy)
-        data.set("permissions", this.pagePermissions)
+        this.creating = true
+        const formData = new FormData()
+        formData.set("name", this.pageName)
+        formData.set("display_name", this.pageDisplayName)
+        formData.set("private", this.pagePrivate)
+        formData.set("permissions", this.pagePermissions)
 
-        this.$axios.post("/newPage", data, {headers: {"X-CSRFToken": getCookie('csrftoken'), "X-Requested-With": "XMLHttpRequest"}})
-          .then(res => {
-            if (res.data["s"]) {
-              window.location = `/page/${res.data["name"]}`
-            } else {
-              if (res.data["m"] === this.nameTakenText) this.TAKEN_PAGE_NAMES.push(data.get("name").toLowerCase())
-              this.invalidateForm(res.data["m"])
+        try {
+          const { data } = await this.$axios.post("/new_page", formData)
+          if (data.success) {
+            $("#newMemePage").modal('hide')
+            await this.addPageToAuthUser(formData)
+            this.$router.push(`/page/${data.name}`)
+          } else {
+            if (data.taken) {
+              this.TAKEN_PAGE_NAMES.push(formData.get("name").toLowerCase())
+            } else if (data.maximum) {
+              this.maximum = true
             }
-          })
-          .catch(console.log)
+            this.invalidateForm(data.taken ? this.nameTakenText : data.maximum ? this.maximumText : data.message)
+          }
+          this.pageName = this.pageDisplayName = ""
+          this.pagePrivate = false
+          this.pagePermissions = true
+        } catch (err) {
+          console.log(err)
+        } finally {
+          this.creating = false
+        }
       }
+    },
+    addPageToAuthUser(formData) {
+      const new_moderating = this.$auth.user.moderating.map(page => {
+        return Object.fromEntries(Object.entries(page))
+      })
+      new_moderating.push({
+        name: formData.get("name"),
+        dname: formData.get("display_name"),
+        private: JSON.parse(formData.get("private"))
+      })
+      this.$auth.setUser(Object.assign({}, this.$auth.user, {moderating: new_moderating}))
+      // this.$store.commit("auth/newMemePage", {
+      //   name: formData.get("name"),
+      //   dname: formData.get("display_name"),
+      //   private: JSON.parse(formData.get("private"))
+      // })
     }
   }
 }
 </script>
-
-<style>
-
-</style>
