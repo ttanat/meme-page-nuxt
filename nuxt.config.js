e22206ab3030b9abdcc26168766139429bd02e99
Tanat <ttanat@protonmail.com>
2020-06-02 04:58:06 +0700

    Don't show progress bar on axios requests

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,8 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "http://127.0.0.1:8000",
+    progress: false
   },
   /*
   ** Auth module configuration
