df27d5147d27dc66815786f146c9bb9864788e36
Tanat <ttanat@protonmail.com>
2020-06-16 05:01:31 +0700

    Add 1 to sub count + change description style + add mods link

--- pages/page/_name/index.vue
+++ pages/page/_name/index.vue
@@ -34,7 +34,8 @@
           <!-- Page stats -->
           <table class="mb-2 w-100">
             <tr>
-              <td class="page-stat" id="sub-count">{{ formatNumber(page.subs) }}</td>
+              <!-- Plus one to include admin -->
+              <td class="page-stat" id="sub-count">{{ formatNumber(page.subs + 1) }}</td>
               <td class="page-stat" id="post-count">{{ formatNumber(page.num_posts) }}</td>
             </tr>
             <tr>
@@ -58,7 +59,7 @@
         <!-- Right column -->
         <div class="col-md-4 col-lg-3 col-xl-3" id="right">
           <div v-if="show" style="width: 260px;">
-            <div class="mb-4 p-2 bg-dark" style="border-radius: 3px;">
+            <div class="mb-4 p-2" id="page-description">
               <BioDescription
                 v-if="is_page_admin"
                 :is-page-admin="is_page_admin"
@@ -66,12 +67,25 @@
                 add-text="page description"
                 @description-change-event="changeDescription"
               />
-              <span v-else id="bio">{{ page.description }}</span>
+              <span v-else id="description">{{ page.description }}</span>
             </div>
             <img v-if="page.num_posts && show" class="ad" src="~/assets/ad.png" alt="Advertisement">
             <div v-if="show" class="list-group mt-4 mb-5">
-              <div class="list-group-item list-group-item-action py-2" style="background: darkgreen;color: white;">Moderators</div>
-              <nuxt-link :to="'/user/'+page.admin" class="list-group-item list-group-item-action py-2" no-prefetch>{{ page.admin }} (admin)</nuxt-link>
+              <nuxt-link
+                :to="'/page/'+page.name+'/moderators'"
+                class="list-group-item list-group-item-action py-2"
+                style="background: darkgreen;color: white;"
+                no-prefetch
+              >
+                Moderators
+              </nuxt-link>
+              <nuxt-link
+                :to="'/user/'+page.admin"
+                class="list-group-item list-group-item-action py-2"
+                no-prefetch
+              >
+                {{ page.admin }} (admin)
+              </nuxt-link>
               <nuxt-link
                 v-for="mod in page.moderators"
                 :key="mod"
@@ -159,11 +173,16 @@ main {
   color: grey;
   font-size: 14px;
 }
-#bio {
+#description {
   font-size: 14px;
 }
+#page-description {
+  border: 1px solid #333;
+  border-radius: 3px;
+  background: #202020;
+}
 .item {
-  border: 1px solid #333333;
+  border: 1px solid #333;
   border-radius: 7px;
 }
 .thumbs {
