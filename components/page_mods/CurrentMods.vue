4535192d9c55df5b3f8eb841703567e79713dbeb
Tanat <ttanat@protonmail.com>
2020-06-16 05:03:15 +0700

    New components that show moderators

---
+++ components/page_mods/CurrentMods.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <ModItem v-for="mod in moderators" :key="mod.username" :mod="mod" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ModItem from '~/components/page_mods/ModItem'
+
+export default {
+  name: 'CurrentMods',
+  components: {
+    ModItem
+  },
+  data() {
+    return {
+      moderators: [{username: "max"}, {username: "jane"}, {username: "moseby"}, {username: "kevin"}, {username: "allison"}],
+      next: `/api/page/${this.$route.params.name}/moderators?page=1`,
+      loading: false
+    }
+  }
+}
+</script>
---
+++ components/page_mods/ModItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-md-6 col-lg-3 px-0">
+    <div class="media p-2 mb-4">
+      <!-- User image or icon -->
+      <nuxt-link :to="'/user/'+mod.username" class="user-icon" target="_blank" no-prefetch>
+        <img v-if="mod.image" :src="mod.image" class="mr-2 pointer rounded-circle">
+        <font-awesome-icon v-else :icon="['fas', 'user']" class="mr-2 pointer" />
+      </nuxt-link>
+      <!-- Username -->
+      <div class="media-body">
+        <nuxt-link :to="'/user/'+mod.username" target="_blank" no-prefetch>
+          {{ mod.username }}<!--&ensp;<font-awesome-icon :icon="['fas', 'external-link-alt']" class="ext-link" />-->
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModItem',
+  props: {
+    mod: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.media {
+  width: 90%;
+  border: 1px solid #333;
+  border-radius: 3px;
+  background: #151515;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+}
+.user-icon {
+  color: lightgrey !important;
+}
+.ext-link {
+  font-size: 12px;
+}
+</style>
