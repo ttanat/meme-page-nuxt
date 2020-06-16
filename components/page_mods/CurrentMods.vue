81c89c2fdfc11055030833e5bae37750c339c925
Tanat <ttanat@protonmail.com>
2020-06-16 18:33:08 +0700

    Move data and methods to CurrentMods component

--- components/page_mods/CurrentMods.vue
+++ components/page_mods/CurrentMods.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <ModItem v-for="mod in moderators" :key="mod.username" :mod="mod" />
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+        <ModItem
+          v-for="mod in moderators"
+          :key="mod.username"
+          :mod="mod"
+          @mod-select-event="numSelected++"
+          @mod-unselect-event="numSelected--"
+        />
+      </div>
     </div>
+    <!-- Spinner and load more button -->
+    <div v-if="loading" id="spinner" class="mt-3"><font-awesome-icon :icon="['fas', 'circle-notch']" spin /></div>
+    <div v-else-if="next"><small class="pointer" @click="loadMore">Load more</small></div>
+    <button v-if="numSelected" @click="removeMods" class="btn btn-sm btn-danger mt-3">Remove</button>
   </div>
 </template>
 
@@ -16,10 +28,65 @@ export default {
   },
   data() {
     return {
-      moderators: [{username: "max"}, {username: "jane"}, {username: "moseby"}, {username: "kevin"}, {username: "allison"}],
-      next: `/api/page/${this.$route.params.name}/moderators?page=1`,
-      loading: false
+      // moderators: [{username: "max"}, {username: "jane"}, {username: "moseby"}, {username: "kevin"}, {username: "allison"}],
+      moderators: [],
+      next: `${this.$axios.defaults.baseURL}/api/moderators/${this.$route.params.name}?page=1`,
+      loading: false,
+      numSelected: 0
+    }
+  },
+  created() {
+    this.loadMore()
+  },
+  methods: {
+    async loadMore() {
+      if (this.next === null) return false
+      this.loading = true
+      try {
+        const { data } = await this.$axios.get(this.next)
+        this.moderators.push(...data.results)
+        if (data.lid) {
+          const url = new URL(this.next)
+          url.searchParams.set("lid", data.lid)
+          this.next = url.href
+        } else {
+          this.next = null
+        }
+      } catch (err) {
+        console.log(err)
+      } finally {
+        this.loading = false
+      }
+    },
+    incrementNumSelected() {
+      this.numSelected++
+    },
+    decrementNumSelected() {
+      this.numSelected--
+    },
+    removeMods() {
+      const selected = this.$children.filter(c => c.selected).map(c => c.mod.username)
+      if (!confirm(`Are you sure you want to remove ${selected.length > 1 ? "these moderators" : selected[0]}`)) return false
+      this.moderators = this.moderators.filter(m => !selected.includes(m.username))
+      this.numSelected = 0
+      this.$toast.info(`${selected.length > 1 ? `${selected.length} moderators` : selected[0]} removed`, {
+        position: 'top-center',
+        duration: 1500
+      })
     }
   }
 }
 </script>
+
+<style scoped>
+a:not(.btn) {
+  color: royalblue;
+  font-size: 15px;
+}
+#spinner {
+  font-size: large;
+}
+small {
+  color: royalblue;
+}
+</style>
--- pages/page/_name/moderators.vue
+++ pages/page/_name/moderators.vue
@@ -1,68 +1,37 @@
 <template>
-  <main class="px-3 px-md-4 px-xl-5">
-    <!-- Back button -->
-    <div>
-      <nuxt-link :to="'/page/'+$route.params.name" class="btn btn-secondary btn-sm mb-4" no-prefetch>
-        Back
-      </nuxt-link>
+  <main>
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+
+        <div class="col-md-11 mb-3">
+          <h4>
+            Moderators
+            <nuxt-link class="btn btn-sm btn-secondary float-right d-none d-lg-inline" :to="'/page/'+$route.params.name">Go back</nuxt-link>
+          </h4>
+        </div>
+
+        <SettingsSidebar />
+        <div class="col-md-9">
+          <h5>Add Moderators</h5>
+          <div style="background:white">woeijf</div>
+          <h5>Current Moderators</h5>
+          <CurrentMods />
+        </div>
+
+      </div>
     </div>
-    <h5>Moderators</h5>
-    <CurrentMods />
-    <!-- Spinner and load more button -->
-    <div v-if="loading" id="spinner" class="mt-3"><font-awesome-icon :icon="['fas', 'circle-notch']" spin /></div>
-    <div v-else-if="next"><small class="pointer" @click="loadMore">Load more</small></div>
   </main>
 </template>
 
 <script>
+import SettingsSidebar from '~/components/page_settings/SettingsSidebar'
 import CurrentMods from '~/components/page_mods/CurrentMods'
 
 export default {
   middleware: 'custom-auth',
   components: {
+    SettingsSidebar,
     CurrentMods
-  },
-  data() {
-    return {
-      
-    }
-  },
-  created() {
-    // this.loadMore()
-  },
-  methods: {
-    async loadMore() {
-      if (this.next === null) return false
-      this.loading = true
-      try {
-        const { data } = await this.$axios.get(this.next)
-        this.moderators.push(...data.results)
-        if (data.next) {
-          const url = new URL(this.next)
-          url.searchParams.set("page", data.next)
-          this.next = url.href
-        } else {
-          this.next = null
-        }
-      } catch (err) {
-        console.log(err)
-      } finally {
-        this.loading = false
-      }
-    }
   }
 }
 </script>
-
-<style scoped>
-a:not(.btn) {
-  color: royalblue;
-  font-size: 15px;
-}
-#spinner {
-  font-size: large;
-}
-small {
-  color: royalblue;
-}
-</style>
