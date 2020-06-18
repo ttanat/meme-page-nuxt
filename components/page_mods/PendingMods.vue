8c1fc11b4d6135d0cbd0c8e96770c775c23104d7
Tanat <ttanat@protonmail.com>
2020-06-18 18:30:54 +0700

    Add remove button

--- components/page_mods/PendingMods.vue
+++ components/page_mods/PendingMods.vue
@@ -11,6 +11,10 @@
         />
       </div>
     </div>
+    <button v-if="numSelected" @click="removePending" class="btn btn-sm btn-danger mb-4">
+      <font-awesome-icon v-if="removing" :icon="['fas', 'circle-notch']" />
+      <template v-else>Remove</template>
+    </button>
   </div>
 </template>
 
@@ -26,6 +30,35 @@ export default {
     pending: {
       type: Array,
       required: true
+    }
+  },
+  data() {
+    return {
+      numSelected: 0,
+      removing: false
+    }
+  },
+  methods: {
+    removePending() {
+      const selected = this.$children.filter(c => c.selected).map(c => c.username)
+      if (!selected.length) return false
+      if (confirm(`Are you sure you want to remove ${selected.length > 1 ? "these invites" : "this invite"}?`)) {
+        this.removing = true
+        const query_params_string = selected.map(s => `username=${s}`).join('&')
+        this.$axios.delete(`/api/mods/pending/${this.$route.params.name}?${query_params_string}`)
+          .then(res => {
+            if (res.status === 204) {
+              this.$emit("remove-mods-event", selected, "pending")
+              this.numSelected = 0
+              this.$toast.info(`${selected.length > 1 ? `${selected.length} moderators` : selected[0]} removed`, {
+                position: 'top-center',
+                duration: 1500
+              })
+            }
+          })
+          .catch(console.log)
+          .finally(() => this.removing = false)
+      }
     }
   }
 }
