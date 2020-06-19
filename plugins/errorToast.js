a311e739347f7047c8f1d1d4cf2b7eba9ae79b97
Tanat <ttanat@protonmail.com>
2020-06-19 14:47:58 +0700

    Don't display error message that is too long

--- plugins/errorToast.js
+++ plugins/errorToast.js
@@ -3,10 +3,14 @@ import Vue from 'vue'
 Vue.mixin({
   methods: {
     errorToast(message, position='top-center', duration=1500) {
-      this.$toast.error(message, {
-        position,
-        duration
-      })
+      if (message.length > 80) {
+        console.log(message)
+      } else {
+        this.$toast.error(message, {
+          position,
+          duration
+        })
+      }
     }
   }
 })
