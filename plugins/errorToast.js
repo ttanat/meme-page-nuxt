9dee860ee14e44a3a84ff6109285566d8b93881f
Tanat <ttanat@protonmail.com>
2020-06-29 17:56:43 +0700

    Remove position argument

--- plugins/errorToast.js
+++ plugins/errorToast.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 
 Vue.mixin({
   methods: {
-    errorToast(message, position='top-center', duration=1500) {
+    errorToast(message, duration=1500) {
       if (message.length > 80) {
         console.log(message)
       } else {
         this.$toast.error(message, {
-          position,
+          position: 'top-center',
           duration
         })
       }
--- plugins/successToast.js
+++ plugins/successToast.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 
 Vue.mixin({
   methods: {
-    successToast(message, position='top-center', duration=1500) {
+    successToast(message, duration=1500) {
       this.$toast.success(message, {
-        position,
+        position: 'top-center',
         duration
       })
     }
