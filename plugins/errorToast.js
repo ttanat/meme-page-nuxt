b600175186f0914ed44d077ed7626db702a34d3a
Tanat <ttanat@protonmail.com>
2020-12-15 02:21:34 +0000

    Longer toast

--- plugins/errorToast.js
+++ plugins/errorToast.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 Vue.mixin({
   methods: {
-    errorToast(message, duration=1500) {
+    errorToast(message, duration=2000) {
       if (message.length > 80) {
         console.log(message)
       } else {
