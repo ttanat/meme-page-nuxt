551f8b1cc2916a9fadb50a1a7069e8298f046db8
Tanat <ttanat@protonmail.com>
2020-12-29 01:29:20 +0000

    Update style for when no comments

--- components/profile/MyComments.vue
+++ components/profile/MyComments.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid px-3">
+  <div class="container-fluid" :class="[!no_content && comments.length ? 'px-3' : 'px-0']">
     <MyComment v-for="comment in comments" :key="comment.uuid" :comment="comment" />
     <div v-if="loading"><font-awesome-icon :icon="['fas', 'circle-notch']" spin /></div>
     <div v-if="no_content && !comments.length" class="profile-empty">No comments yet</div>
@@ -24,9 +24,8 @@ export default {
       no_content: false
     }
   },
-  async mounted() {
-    await this.loadMore()
-    if (!this.comments.length) this.no_content = true
+  mounted() {
+    this.loadMore()
   },
   methods: {
     loadMore() {
@@ -38,8 +37,22 @@ export default {
           this.next = res.data.next
         })
         .catch(console.log)
-        .finally(() => this.loading = false)
+        .finally(() => {
+          this.loading = false
+          if (!this.comments.length) this.no_content = true
+        })
     }
   }
 }
 </script>
+
+<style scoped>
+.profile-empty {
+  text-align: center;
+  padding: 100px;
+  background-color: #090909;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+}
+</style>
