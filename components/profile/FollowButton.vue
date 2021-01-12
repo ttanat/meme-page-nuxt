e47706c93861b7078a2b68498920bd31b6017ad1
Tanat <ttanat@protonmail.com>
2021-01-12 16:11:55 +0000

    Limit number that user can follow to 1000

--- components/profile/FollowButton.vue
+++ components/profile/FollowButton.vue
@@ -35,7 +35,7 @@ export default {
   computed: {
     buttonStyle() {
       return {
-        padding: this.small ? "0 2px 0 2px" : "5px",
+        padding: this.small ? "0 2px" : "5px",
         paddingTop: this.small ? "0px" : "2px",
         paddingBottom: this.small ? "0px" : "2px",
         fontSize: this.small ? "12px" : ""
@@ -45,12 +45,18 @@ export default {
   methods: {
     follow() {
       if (this.checkAuth()) {
-        this.$axios.get(`/api/follow/${this.username || this.$route.params.username}`)
-          .then(({ data }) => {
-            this.$emit("following-changed-event", data.following)
-            this.changeNumFollowing(data.following ? 1 : -1)
-          })
-          .catch(this.displayError)
+        if (this.$auth.user.stats.num_following >= 1000) {
+          this.displayError("Cannot follow more than 1000 users")
+        } else {
+          this.$axios.get(`/api/follow/${this.username || this.$route.params.username}`)
+            .then(({ data }) => {
+              this.$emit("following-changed-event", data.following)
+              this.changeNumFollowing(data.following ? 1 : -1)
+            })
+            .catch(err => {
+              this.displayError(err.response.data.length <= 100 ? err.response.data : err.message)
+            })
+        }
       }
     },
     changeNumFollowing(change) {
