3dee788e7820a27e371fd6e5b786002c573ab667
Tanat <ttanat@protonmail.com>
2021-01-13 03:37:16 +0000

    Create fake admin page

---
+++ pages/admin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3 v-if="loading"><font-awesome-icon :icon="['fas', 'circle-notch']" spin />&ensp;Loading admin site</h3>
+    <img v-else src="~/assets/sike.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'plain',
+  data() {
+    return {
+      loading: true
+    }
+  },
+  head() {
+    return {
+      title: "Admin",
+      meta: [
+        { hid: 'robots', name: 'robots', content: 'noindex, noimageindex' }
+      ]
+    }
+  },
+  mounted() {
+    setTimeout(() => {this.loading = false}, 2000)
+  }
+}
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+h3 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  word-spacing: 1px;
+}
+img {
+  height: 100vh;
+}
+</style>
